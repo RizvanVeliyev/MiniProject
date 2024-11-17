71294964cfe6af40f292aa7b1e833e19a79b88bb
RizvanVeliyev <veliyevrizvan250@gmail.com>
2024-11-18 02:53:32 +0400

    Artiq servislerde homewviewmodelde gelir,, veindex viewsunda menimservis modelim gorunur

--- Pustok.BLL/UI/Services/HomeManager.cs
+++ Pustok.BLL/UI/Services/HomeManager.cs
@@ -10,12 +10,14 @@ namespace Pustok.BLL.UI.Services
         private readonly IProductService _productService;
         private readonly ICategoryService _categoryService;
         private readonly ISliderService _sliderService;
+        private readonly IServiceService _serviceService;
 
-        public HomeManager(IProductService productService, ICategoryService categoryService,ISliderService sliderService)
+        public HomeManager(IProductService productService, ICategoryService categoryService,ISliderService sliderService, IServiceService serviceService)
         {
             _productService = productService;
             _categoryService = categoryService;
-            _sliderService=sliderService;
+            _sliderService = sliderService;
+            _serviceService = serviceService;
         }
 
         public async Task<HomeViewModel> GetHomeViewModelAsync()
@@ -24,11 +26,14 @@ namespace Pustok.BLL.UI.Services
             //var products = await _productService.GetAllAsync(include: x => x.Include(y => y.ProductImages!).Include(c => c.ProductCategories)!.ThenInclude(t => t.Category)!);
             var products = (await _productService.GetAllAsync(predicate: p => !p.IsDeleted, include: p => p.Include(p => p.ProductImages))).ToList();
             var sliders = await _sliderService.GetAllAsync(predicate: p => !p.isDeleted);
+            var services = await _serviceService.GetAllAsync(predicate: p => !p.IsDeleted);
+
             var model = new HomeViewModel
             {
                 Sliders=sliders,
                 Categories = categories,
-                Products = products
+                Products = products,
+                Services= services
             };
 
             return model;
--- Pustok.BLL/UI/ViewModels/HomeViewModel.cs
+++ Pustok.BLL/UI/ViewModels/HomeViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using Pustok.BLL.ViewModels.CategoryViewModels;
 using Pustok.BLL.ViewModels.ProductViewModels;
+using Pustok.BLL.ViewModels.ServiceViewModels;
 using Pustok.BLL.ViewModels.SliderViewModels;
+using System.Security.Policy;
 
 namespace Pustok.BLL.UI.ViewModels
 {
@@ -9,5 +11,6 @@ namespace Pustok.BLL.UI.ViewModels
         public List<ProductViewModel>? Products { get; set; }
         public List<CategoryViewModel>? Categories { get; set; }
         public List<SliderViewModel>? Sliders { get; set; }
+        public List<ServiceViewModel> Services { get; set; }
     }
 }
