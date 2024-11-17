5b2fe224dc5e41a9bd2aa248959b939d97b8bab0
RizvanVeliyev <veliyevrizvan250@gmail.com>
2024-11-18 02:45:08 +0400

    Demeli mappinde deyisiklik eledim chage role voewsunda userid gonderdim falan opsim isleyir 90 faiz yeni isimi gorur. Indi bizde en esas qaldi, paginatin shopda birde filterlama category ucun ve 3product hissesi, Bundan elave setting update ve details crud, sonra services frontoback etmek

--- Pustok.BLL/AutoMapper/MappingProfile.cs
+++ Pustok.BLL/AutoMapper/MappingProfile.cs
@@ -57,7 +57,9 @@ namespace Pustok.BLL.AutoMapper
 
             CreateMap<AppUser, RegisterViewModel>().ReverseMap();
             CreateMap<AppUser, LoginViewModel>().ReverseMap();
-            CreateMap<AppUser,UserViewModel>().ReverseMap();
+            //CreateMap<AppUser,UserViewModel>().ReverseMap();
+            CreateMap<AppUser, UserViewModel>().ForMember(dest => dest.Roles, opt => opt.Ignore()).ReverseMap();
+
             CreateMap<AppUser, ChangeRoleViewModel>().ReverseMap();
 
         }
